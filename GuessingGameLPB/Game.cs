d95b1836d0e28c54fd9f718e42c4e21dda427451
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2021-02-11 13:39:55 -0600

    first commit

---
+++ GuessingGameLPB/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessingGameLPB
+{
+    class Game
+    {
+        Random randNum = new Random();
+
+
+
+
+        public int RandomNumber(int min, int max)
+        {
+
+            return randNum.Next(min, max);
+
+
+        }
+
+        
+
+        
+
+
+
+
+
+
+    }
+}
